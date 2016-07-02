c2adbf75813da90bf7dbff364bcc14feecb8de41
yexiaodong <2283138757@qq.com>
2016-07-02 11:36:06 +0800

    2016.07.02

---
+++ MyTest/Test-Html/调试-Chrome/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by fits on 2016/6/26.
+ */
+window.onload = function () {
+    alert("d");
+}
+function test1(){
+    var num = 6;
+    alert("实时编辑");
+}
---
+++ MyTest/Test-Html/调试-Chrome/实时代码编辑.js
@@ -0,0 +1,9 @@
+/**
+ * Created by fits on 2016/6/26.
+ */
+window.onload = function () {
+    alert("d");
+}
+function test1(){
+    alert("实时编辑6");
+}
